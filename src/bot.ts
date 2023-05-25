baffd183295a2ffbf9674725ed8f8a882c184df8
¨João Gonçalves¨ <joao.n.d.goncalves@gmail.com>
2023-05-25 20:44:45 +0100

    removed .env

--- src/bot.ts
+++ src/bot.ts
@@ -8,6 +8,4 @@ client.once('ready', () => {
   console.log('Ready!');
 });
 
-client.login(
-  'Njg5NDU4MDMxMjEzNTQzNTU5.GWKjpH.Yr_GNIFXb1GMN_5OhRVjEQjk0K326H1Vm5useo'
-);
+client.login(process.env.TOKEN);
